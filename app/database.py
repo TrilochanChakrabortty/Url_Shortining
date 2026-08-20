3a3ed4990e411b8fa0b6c756287886112e65ba55
Trilochan <trilochanchqkrabortty@gmail.com>
2026-08-20 13:08:03 +0530

    Remove environment secrets and update gitignore

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:trilochan@localhost:3306/url_shortener"
+DATABASE_URL = "mysql+pymysql://root:username@localhost:3306/url_shortener"
 
 engine = create_engine(DATABASE_URL)
 
